775c294497309462add01f54fc4b6e1b6bdb4e09
mingdodev <mingdo.dev@gmail.com>
2026-01-01 16:34:02 +0900

    Users Microservice와 Spring Cloud Gateway 연동

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 @Slf4j
 public class UserController {
     private final Environment env;
